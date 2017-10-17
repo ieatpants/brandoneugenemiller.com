8f723e413f770c53f2ec9b6c7206108f441ab68f
brandon <brandon.eugene.miller@gmail.com>
2017-10-17 13:00:25 +0100

    Update dom.js
    temporarily removed conditional for loading skills bar animation

--- js/dom.js
+++ js/dom.js
@@ -82,13 +82,13 @@ $(window).scroll(function(){
           var winT = $(window).scrollTop(),
             winH = $(window).height(),
             skillsT = skillsDiv.offset().top;
-          if (winT + winH > skillsT) {
+          //if (winT + winH > skillsT) {
             $('.skills').each(function(){
               $(this).find('.bar').animate({
                 width:$(this).attr('data-percent')
               },2200);
             });
-          }
+          /}
           });
 
                     /* light box */
